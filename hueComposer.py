e47932de4036fa4bd527d11e1c391f5a0a1f7b5a
sabjorn <info@sabjorn.net>
2017-05-28 20:19:39 -0700

    began to stub and build out config file support. YAML library was added as a support package.
    automatically generated python requirements file using pipreqs
    
    config appears to work properly and has the ability for multiple images to be played back at different rates
    
    tested the config file with multiple lights to make sure it works. It does. Also added some more error handling and logging
    
    added example config file for reference

--- hueComposer.py
+++ hueComposer.py
@@ -1,52 +1,53 @@
 #!/usr/bin/python
 
 # Copyright (c) 2016 Steven A. Bjornson
-# Permission is hereby granted, free of charge, to any person obtaining a copy 
-# of this software and associated documentation files (the "Software"), to deal 
-# in the Software without restriction, including without limitation the rights to 
-# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of 
-# the Software, and to permit persons to whom the Software is furnished to do so, 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights to
+# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+# the Software, and to permit persons to whom the Software is furnished to do so,
 # subject to the following conditions:
 #
-# The above copyright notice and this permission notice shall be included in all copies 
+# The above copyright notice and this permission notice shall be included in all copies
 # or substantial portions of the Software.
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
-# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
-# PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
-# FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+# PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+# FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
 # ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 ## Info
 # The goal of this software is to fascilitate the creation
-# of complex light patterns using a large array of 
+# of complex light patterns using a large array of
 # Philips Hue bulbs.
-# 
+#
 # An image is imported with PIL and the pixels are mapped
 # to the bulbs.
 # The x-axis being time and y-axis being bulb index
 
 import argparse
-import glob #list all images
+import glob  # list all images
 from time import sleep
 import logging
+
+import yaml
 
 from PIL import Image
 import numpy as np
 from scipy.misc import imresize
 
-from phue import Bridge #https://github.com/studioimaginaire/phue
+from phue import Bridge  # https://github.com/studioimaginaire/phue
 # turns out Hue [0, 65535] and Saturation [0, 254] are available
 # as properties of the lights
 from hueColour import Converter
 
 if __name__ == "__main__":
-    logging.basicConfig(filename='/home/pi/hueComposer/hue.log', level=logging.INFO, format='%(asctime)s %(message)s')
-    logging.info('Started')
-
     parser = argparse.ArgumentParser()
-    parser.add_argument("--ip", type=str, default='192.168.0.100', 
+    parser.add_argument("--ip", type=str, default='192.168.1.64',
                         help='hue hub ip address')
-    parser.add_argument("--input", "--i", type=str, default='/home/pi/imgs',
+    parser.add_argument("--username", "-u", type=str,
+                        help="Hue hub username.")
+    parser.add_argument("--input", "-i", type=str, default='./',
                         help='Input image directory')
     parser.add_argument("--rate", "--r", type=float, default=1/5.,
                         help='Rate in ms to step between pixel values')
@@ -54,33 +55,64 @@ if __name__ == "__main__":
                         help='Transition time in seconds')
     parser.add_argument("--numlights", "--n", type=float, default=30,
                         help='Number of active lights')
-
+    parser.add_argument("--config", "-c", type=str,
+                        help='run with config files')
 
     args = parser.parse_args()
 
-    b = Bridge(args.ip)
-    b.username = "Ga3Q2uvDeCQBKt3srFKRolzLRaQkgpIm8tENVY0-"
-    b.connect()
+    logging.basicConfig(filename='./hue.log', level=logging.INFO, format='%(asctime)s %(message)s')
+    logging.info('Started')
 
-
-    while True:
+    config_flag = args.config is not None
+    cfg = None
+    if(config_flag):
         try:
-            lights = b.lights
-        except:
-            continue
-        break
+            with open(args.config, 'r') as ymlfile:
+                cfg = yaml.load(ymlfile)
+                config = cfg['config']
+                args.numlights = config['lights']
+                args.username = config['username']
+                args.ip = config['ip']
+        except IOError:
+            logging.exception("Could not open file: {}".format(args.config))
+            exit(1)
+
+    b = Bridge(args.ip)
+
+    #not fully tested!
+    if(args.username is None):
+        b.connect()
+        args.username = b.username
+        logging.info("Username: {}".format(b.username))
+    else:
+        b.username = args.username
+
+    # while True:
+    #     try:
+    #         lights = b.lights
+    #     except:
+    #         continue
+    #     break
 
     conv = Converter()
 
-    #make subset of lights which are active (a map)
-    
-    #make ordered list of images in directory
-    imgs = glob.glob("{}/*.png".format(args.input))
+    # make subset of lights which are active (a map)
+    # make ordered list of images in directory
+    imgs = []
+    images = None
+    if(config_flag):
+        images = cfg['images']
+        for k, v in images.items():
+            imgs.append(v['filename'])
+    else:
+        imgs = glob.glob("{}/*.png".format(args.input))
 
     try:
+        lights = b.lights
         while True:
             for i, names in enumerate(imgs):
                 logging.info(names)
+
                 for x in lights:
                     x.transitiontime = args.transition
                     x.on = True
@@ -88,6 +120,13 @@ if __name__ == "__main__":
                 img = Image.open(names)
                 img = np.asarray(img)
 
+                if(config_flag):
+                    args.transition = images[i]['transition']
+                    args.rate = (images[i]['time'] - (img.shape[1] * args.transition)) / img.shape[1]
+                    if(args.rate < 1):
+                        args.rate = 10
+                        logging.info("Rate too slow")
+                        print("rate too slow")
                 #shrink Y-axis to size of array
                 step = 1
                 if(img.shape[0] > args.numlights):
@@ -104,48 +143,23 @@ if __name__ == "__main__":
 
                 for x in np.arange(img.shape[1]):
                     for y in np.arange(0, args.numlights):
-                        bri = int(np.average(img[y,x,:]))
+                        bri = int(np.average(img[y, x, :]))
                         lights[y].xy = conv.rgbToCIE(img[y*step, x, 0], img[y*step, x, 1], img[y*step, x, 2])
                         lights[y].bri = bri
                     #print(x/float(img.shape[1]))
 
                     sleep(args.rate)
 
+                # image finished animation
                 for i, x in enumerate(lights):
                     if(i % 2):
                         x.on = False
 
-
                 sleep(10)
-
-                            #command =  {'xy': conv.rgbToCIE(img[y, x, 0], img[y, x, 1], img[y, x, 2]), 'bri' : bri}
-                        # else:
-                        #     command = {'On': False}
-                        #commands.append(command)
-                    # for y, command in enumerate(commands):
-                    #     b.set_light(y+1, command) # best to shoot these all out like this?
-
-    except KeyboardInterrupt:
+    except KeyboardInterrupt, e:
         #some sort of cleanup
-        0
+        logging.info("Keyboard Interrupt")
+    except Exception, e:
+        logging.info("Something bad happened")
 
     logging.info('Finished')
-
-
-    # try:
-    #     while 1:
-    #         #load image
-
-    #         #step through image
-    #         """
-    #         set_light(self, light_id, parameter, value=None, transitiontime=None):
-    #         Adjust properties of one or more lights.
-    #         light_id can be a single lamp or an ARRAY of lamps!
-    #         parameters: 'on' : True|False , 'bri' : 0-254, 'sat' : 0-254, 'ct': 154-500
-    #         """
-
-    #         #back to load image
-
-    # except KeyboardInterrupt:
-    #     #some sort of cleanup
-    #     0
